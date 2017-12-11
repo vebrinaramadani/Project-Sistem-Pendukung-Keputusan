a9590617d90ab1aa9fd83fafb9207c191fe5784c
vebrinaramadani <vebrinaramadani@gmail.com>
2017-12-11 15:33:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'header.php';
+?>
+
+<html>
+	<body>
+		<div class="container">
+			<h1>
+				<center>Sistem Pendukung Keputusan Pemilihan Sekolah 
+				<p>Menggunakan Metode SAW</center></p>
+			</h1>
+   		<p>
+   			<center><span style = "font-size:20px;"> Sistem ini dirancang untuk memilih sekolah yang tepat, maka perlu dirancang sebuah mekanisme yang dapat membantu pemilihan sekolah. . SPK menggunakan metode Simple Additive Weighting (SAW) digunakan untuk menyelesaikan suatu persoalan yang tidak terstuktur secara sederhana. Sehingga dengan menerapkan metode tersebut, diharapkan dapat membantu proses pengambilan keputusan pemilihan sekolah secara tepat</span></center>
+   		</p>
+   		</div>
+   			<br></br></center>
+			<center><img width=900 height=400 src='images/school.png' /></center>
+	</body>
+		<br></br>
+</html>
+
+<?php
+include_once 'footer.php';
+?>
